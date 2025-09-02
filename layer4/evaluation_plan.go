4b13adf7f6d996739a1f5d0b2b8d1767361ebee5
Jennifer Power <barnabei.jennifer@gmail.com>
2025-09-02 16:21:52 -0400

    chore: add yaml struct tags to EvaluationPlan
    Signed-off-by: Jennifer Power <barnabei.jennifer@gmail.com>

--- layer4/evaluation_plan.go
+++ layer4/evaluation_plan.go
@@ -1,63 +1,63 @@
 package layer4
 
 type EvaluationPlan struct {
-	Metadata Metadata `json:"metadata"`
+	Metadata	Metadata	`json:"metadata" yaml:"metadata"`
 
-	Plans []AssessmentPlan `json:"plans"`
+	Plans	[]AssessmentPlan	`json:"plans" yaml:"plans"`
 }
 
 type Metadata struct {
-	Id string `json:"id"`
+	Id	string	`json:"id" yaml:"id"`
 
-	Version string `json:"version,omitempty"`
+	Version	string	`json:"version,omitempty" yaml:"version,omitempty"`
 
-	Author Contact `json:"author"`
+	Author	Contact	`json:"author" yaml:"author"`
 }
 
 type Contact struct {
 	// The contact person's name.
-	Name string `json:"name"`
+	Name	string	`json:"name" yaml:"name"`
 
 	// Indicates whether this admin is the first point of contact for inquiries. Only one entry should be marked as primary.
-	Primary bool `json:"primary"`
+	Primary	bool	`json:"primary" yaml:"primary"`
 
 	// The entity with which the contact is affiliated, such as a school or employer.
-	Affiliation *string `json:"affiliation,omitempty"`
+	Affiliation	*string	`json:"affiliation,omitempty" yaml:"affiliation,omitempty"`
 
 	// A preferred email address to reach the contact.
-	Email *string `json:"email,omitempty"`
+	Email	*string	`json:"email,omitempty" yaml:"email,omitempty"`
 
 	// A social media handle or profile for the contact.
-	Social *string `json:"social,omitempty"`
+	Social	*string	`json:"social,omitempty" yaml:"social,omitempty"`
 }
 
 // AssessmentPlan defines all testing procedures for a control id.
 type AssessmentPlan struct {
-	ControlId string `json:"control-id"`
+	ControlId	string	`json:"control-id" yaml:"control-id"`
 
-	Assessments []Assessment `json:"assessments"`
+	Assessments	[]Assessment	`json:"assessments" yaml:"assessments"`
 }
 
 // AssessmentPlan defines all testing procedures for a requirement.
 type Assessment struct {
 	// RequirementID is the unique identifier for the requirement being tested
-	RequirementId string `json:"requirement-id"`
+	RequirementId	string	`json:"requirement-id" yaml:"requirement-id"`
 
 	// Procedures defined possible testing procedures to evaluate the requirement.
-	Procedures []AssessmentProcedure `json:"procedures"`
+	Procedures	[]AssessmentProcedure	`json:"procedures" yaml:"procedures"`
 }
 
 // AssessmentProcedure describes a testing procedure for evaluating a Layer 2 control requirement.
 type AssessmentProcedure struct {
 	// Id uniquely identifies the assessment procedure being executed
-	Id string `json:"id"`
+	Id	string	`json:"id" yaml:"id"`
 
 	// Name provides a summary of the procedure
-	Name string `json:"name"`
+	Name	string	`json:"name" yaml:"name"`
 
 	// Description provides a detailed explanation of the procedure
-	Description string `json:"description"`
+	Description	string	`json:"description" yaml:"description"`
 
 	// Documentation provides a URL to documentation that describes how the assessment procedure evaluates the control requirement
-	Documentation string `json:"documentation,omitempty"`
+	Documentation	string	`json:"documentation,omitempty" yaml:"documentation,omitempty"`
 }
